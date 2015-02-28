8d5740128e05b5f7dee685e5ef6070fbe8043fed
Mitchell Raful <mitchellraful@Mitchells-MBP.home>
2015-02-28 15:01:27 -0500

    Front page HTML now generated from template

--- functional_test.py
+++ functional_test.py
@@ -17,19 +17,19 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do listss
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_elements_by_tag_name('h1').text
-        self.assertIn('Tl-Do', header_text )
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text )
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual( inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue( any( row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue( any( row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
         self.fail('Finish the test')
 
 
