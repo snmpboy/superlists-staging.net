a4629eb20c4a2faae5b0a9272bd87828e9201e6e
Mitchell Raful <mitchellraful@Mitchells-MBP.home>
2015-03-01 14:22:33 -0500

    Updating tests

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import unittest.case
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -29,8 +29,9 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue( any( row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.assertIn( '1: Buy peacock feathers', [row.text for row in rows])
         self.fail('Finish the test')
+
 
 
 if __name__ == '__main__':
