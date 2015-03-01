79c55c5ee2d79348d41202ad32d317402bba5730
Mitchell Raful <mitchellraful@Mitchells-MBP.home>
2015-03-01 14:53:37 -0500

    Updating tests to test for multiple list items

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn( row_text, [row.text for row in rows] )
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check its homepage
         self.browser.get('http://localhost:8000')
@@ -22,14 +28,16 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual( inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn( '1: Buy peacock feathers', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly' )
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        #Edith needs to save her list items
         self.fail('Finish the test')
 
 
