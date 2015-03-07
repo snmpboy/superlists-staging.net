0535d8b2bf6f23f6cba413face0768c866b007be
Mitchell Raful <mitchellraful@Mitchells-MBP.home>
2015-03-07 13:27:35 -0500

    Rredirect after POST, database with sqlite and show items in Template

--- functional_test.py
+++ functional_test.py
@@ -30,17 +30,16 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual( inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feather to make a fly' )
+        inputbox.send_keys('Use peacock feathers to make a fly' )
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         #Edith needs to save her list items
         self.fail('Finish the test')
-
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
